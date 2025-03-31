9ff1183b2d083b01da7bcd143a7dfc058f7e9eff
CharbelMaro <126902391+Charbel12323@users.noreply.github.com>
2025-03-31 22:24:36 +0000

    updated url for UI testing

--- src/ui_tests/behave/features/environment.py
+++ src/ui_tests/behave/features/environment.py
@@ -2,24 +2,23 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.common.proxy import Proxy, ProxyType
 
-SERVER = "localhost"
-URL = "http://%s:8080" % SERVER
-
+# Update the server to match the demo app host and include the context path
+SERVER = "demo-app"
+URL = "http://%s:8080/demo" % SERVER
 
 def before_all(context):
     __open_browser(context)
-
 
 def __open_browser(context):
     chrm = context.config.userdata['chromedriver_path']
     
     try:
-        # if there is a proxy, we'll use it.  Otherwise, we won't.
-        requests.get("http://localhost:8888", timeout=0.01)
+        # If there is a proxy, we'll use it. Otherwise, we won't.
+        # Changed the proxy check URL to demo-app as well.
+        requests.get("http://demo-app:8888", timeout=0.01)
 
-        # if there was no exception, we continue here.
-        PROXY = "localhost:8888"
-
+        # If the proxy is available, use it.
+        PROXY = "demo-app:8888"
         proxy = Proxy()
         proxy.proxy_type = ProxyType.MANUAL
         proxy.http_proxy = PROXY
@@ -27,42 +26,28 @@ def __open_browser(context):
         capabilities = webdriver.DesiredCapabilities.CHROME
         proxy.add_to_capabilities(capabilities)
         
-        if (chrm):
+        if chrm:
             context.driver = webdriver.Chrome(desired_capabilities=capabilities, executable_path=chrm)
         else:
             context.driver = webdriver.Chrome(desired_capabilities=capabilities)
         return context.driver
     except:
-        if (chrm):
+        # If proxy is not available, launch Chrome normally.
+        if chrm:
             context.driver = webdriver.Chrome(executable_path=chrm)
         else:
-            # adding the service args as described below will cause Chromedriver
-            # to create a log of the communication between it and the Chrome
-            # browser.  It's eye-opening. 
-            #
-            # for instance:
-	        #   [1568045962.076][INFO]: [e18882b1f2abbda89f232f777f98f686] COMMAND TypeElement {
-	        #      "id": "0.47079920350295135-1",
-	        #      "sessionId": "e18882b1f2abbda89f232f777f98f686",
-	        #      "text": "Byron",
-	        #      "value": [ "B", "y", "r", "o", "n" ]
-	        #   }
-            #context.driver = webdriver.Chrome(service_args=["--verbose","--logepath=C:\\temp\\qc1.log"])
             context.driver = webdriver.Chrome()
         return context.driver
-
 
 def before_scenario(context, scenario):
     __reset_database()
 
-
 def after_all(context):
     __close_browser(context)
-
 
 def __close_browser(context):
     context.driver.close()
 
-
 def __reset_database():
-    requests.get("%s/demo/flyway" % URL)
+    # Reset the database using the demo app URL (the /flyway endpoint is now relative to the demo context)
+    requests.get("%s/flyway" % URL)
