8cc1392c83b010955329337d6c52425eb26b3c2b
CharbelMaro <126902391+Charbel12323@users.noreply.github.com>
2025-03-31 22:45:23 +0000

    updated env

--- src/ui_tests/behave/features/environment.py
+++ src/ui_tests/behave/features/environment.py
@@ -1,19 +1,22 @@
 import requests
+import tempfile
 from selenium import webdriver
 
 SERVER = "demo-app"
-URL = "http://%s:8080/demo" % SERVER
+URL = f"http://{SERVER}:8080/demo"
 
 def before_all(context):
-    context.driver = __open_browser(context)
-
-def __open_browser(context):
+    # Create a unique temporary directory for the Chrome user data
+    user_data_dir = tempfile.mkdtemp()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
+    
+    # Optionally, use a custom chromedriver path if provided in userdata
     chrm = context.config.userdata.get('chromedriver_path', None)
-    # Launch Chrome without any proxy settings
     if chrm:
-        return webdriver.Chrome(executable_path=chrm)
+        context.driver = webdriver.Chrome(executable_path=chrm, options=chrome_options)
     else:
-        return webdriver.Chrome()
+        context.driver = webdriver.Chrome(options=chrome_options)
 
 def before_scenario(context, scenario):
     __reset_database()
@@ -26,4 +29,4 @@ def __close_browser(context):
         context.driver.quit()
 
 def __reset_database():
-    requests.get("%s/flyway" % URL)
+    requests.get(f"{URL}/flyway")
