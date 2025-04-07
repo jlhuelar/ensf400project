0d709adfdaddbb2c74901f7fbdff609b0a187eda
CharbelMaro <126902391+Charbel12323@users.noreply.github.com>
2025-04-07 01:24:14 +0000

    removed comments

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,5 +5,4 @@ echo "Fixing permissions on /var/jenkins_home..."
 chown -R 1000:1000 /var/jenkins_home
 chmod -R 775 /var/jenkins_home
 
-# Execute the original Jenkins entrypoint script
 exec /usr/local/bin/jenkins.sh "$@"
