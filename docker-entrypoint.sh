3d84a2a4abc9acd6c50dcecf8512826885b08ec7
CharbelMaro <126902391+Charbel12323@users.noreply.github.com>
2025-04-07 05:53:07 +0000

    updated docker.sock

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,4 +5,10 @@ echo "Fixing permissions on /var/jenkins_home..."
 chown -R 1000:1000 /var/jenkins_home
 chmod -R 775 /var/jenkins_home
 
+
+
+
+echo "WARNING: Applying insecure permissions (666) to /var/run/docker.sock!"
+chmod 666 /var/run/docker.sock || echo "WARNING: Failed to chmod docker.sock. This command might require root privileges or for the container to be run as --privileged (which is also insecure)."
+
 exec /usr/local/bin/jenkins.sh "$@"
